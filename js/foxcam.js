db64d02d87720aaa575797ca7d15490607d9153b
James Hsieh <cookiej4u@gmail.com>
2013-11-28 03:34:16 +0800

    First Main Screen prototype To be merged with the edit page.

--- js/foxcam.js
+++ js/foxcam.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMComponentsLoaded', function(){
-	//alert("loaded");
+    //alert("loaded");
 });
 
 (function () {
-	$("li.page-toogler").click(function(e){
-		e.preventDefault();
-		var target = $(this).attr('data-page-no');
-		$("div.page").each(function( index ) {
-			if(index == target)
-				$(this).css('left','0');
-		});
-	});
+    $("li.page-toggler").click(function(e){
+        e.preventDefault();
+        var target = $(this).attr('data-page-no');
+        $("div.page").each(function( index ) {
+            if(index == target)
+                $(this).css('left','0');
+        });
+    });
 })();
