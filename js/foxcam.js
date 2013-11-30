f6a0c6ecf55893b40283f54947dceba10278a894
GaryTan <garytan@nisra.net>
2013-12-01 02:20:25 +0800

    without scrollbar

--- js/foxcam.js
+++ js/foxcam.js
@@ -29,4 +29,70 @@ document.addEventListener('DOMComponentsLoaded', function(){
 		$("#collage-footer").css('left','300%');
 		$("#setting-footer").css('left','400%');
 	});
+	$("i#choose-image").click(function(e){
+		e.preventDefault();
+		var canvas = document.getElementById("image-canvas");
+        var context = canvas.getContext("2d");
+		var pick = new MozActivity({
+                name: "pick",
+                data: {
+                    type: ["image/png", "image/jpg", "image/jpeg"]
+                }
+        });
+        pick.onsuccess = function () { 
+                var img = new Image;
+                img.src = URL.createObjectURL(this.result.blob);
+                img.onload = function() {
+                    scale_ratio_h = canvas.height/img.height;
+                    scale_ratio_w = canvas.width/img.width;
+                    alert(scale_ratio_h + " " + scale_ratio_w);
+                    if(img.height > img.width){
+                    	context.drawImage(img,0,0, Math.ceil(canvas.width*(img.width/img.height)),canvas.height);
+                    }else if (img.height < img.width){
+                    	context.drawImage(img,0,0, canvas.width, Math.ceil(canvas.height*(img.height/img.width)));
+                    }else
+                    	context.drawImage(img,0,0, canvas.width, canvas.width);
+                    //alert(canvas.width/img.width);
+                    //var previewHeight = img.height*(canvas.width/img.width);
+                    //context.drawImage(img,0,0);
+                }
+                $("#image-canvas").css('display','block');
+            };
+
+            pick.onerror = function () {
+                
+            };
+	});
+    $(".fa-crop").click(function(e){
+		e.preventDefault();
+		$("#edit-intro").css('visibility','hidden');
+		$("#edit-crop").css('visibility','visible');
+	});
+	$(".fa-repeat").click(function(e){
+		e.preventDefault();
+		$("#edit-intro").css('visibility','hidden');
+		$("#edit-rotate").css('visibility','visible');
+	});
+	$(".fa-magic").click(function(e){
+		e.preventDefault();
+		$("#edit-intro").css('visibility','hidden');
+		$("#edit-effect").css('visibility','visible');
+	});
+	$(".fa-check").click(function(e){
+		e.preventDefault();
+		$("#edit-intro").css('visibility','visible');
+		$("#edit-crop").css('visibility','hidden');
+		$("#edit-rotate").css('visibility','hidden');
+		$("#edit-effect").css('visibility','hidden');
+	});
+	$(".fa-times").click(function(e){
+		e.preventDefault();
+		$("#edit-intro").css('visibility','visible');
+		$("#edit-crop").css('visibility','hidden');
+		$("#edit-rotate").css('visibility','hidden');
+		$("#edit-effect").css('visibility','hidden');
+	});
+	$('#scrollbar').slimScroll({
+        height: '20px'
+    });
 })();
