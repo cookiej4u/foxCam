2878c164096ea214072dad2ea9e50eff000b3f2b
System Administrator <root@RickyChoulteki-MacBook-Air.local>
2013-12-01 23:33:17 +0800

    just for backup.

--- js/foxcam.js
+++ js/foxcam.js
@@ -3,6 +3,14 @@ document.addEventListener('DOMComponentsLoaded', function(){
 });
 
 (function () {
+    var width = 0;
+    var height = 0;
+    var img = new Image;
+    var rotating = false;
+    var canvas = document.getElementById("image-canvas");
+    var canvas_bak = document.createElement("canvas");
+    var ch;
+    var cw;
     $("li.page-toggler").click(function(e){
         e.preventDefault();
         $("#navigator").css('visibility','visible');
@@ -31,8 +39,8 @@ document.addEventListener('DOMComponentsLoaded', function(){
     });
     $("i#choose-image").click(function(e){
         e.preventDefault();
-        var canvas = document.getElementById("image-canvas");
         var context = canvas.getContext("2d");
+        var b_contxet = canvas_bak.getContext("2d");
         var pick = new MozActivity({
             name: "pick",
             data: {
@@ -40,27 +48,59 @@ document.addEventListener('DOMComponentsLoaded', function(){
             }
         });
         pick.onsuccess = function () { 
-            var img = new Image;
             img.src = URL.createObjectURL(this.result.blob);
             img.onload = function() {
                 scale_ratio_h = canvas.height/img.height;
                 scale_ratio_w = canvas.width/img.width;
-                alert(scale_ratio_h + " " + scale_ratio_w);
+                //alert(scale_ratio_h + " " + scale_ratio_w);
                 if(img.height > img.width){
                     context.drawImage(img,0,0, Math.ceil(canvas.width*(img.width/img.height)),canvas.height);
+                    b_contxet.drawImage(img,0,0, Math.ceil(canvas.width*(img.width/img.height)),canvas.height);
+                    canvas_bak.width = Math.ceil(canvas.width*(img.width/img.height));
+                    canvas_bak.height = canvas.height;
                 }else if (img.height < img.width){
                     context.drawImage(img,0,0, canvas.width, Math.ceil(canvas.height*(img.height/img.width)));
+                    b_contxet.drawImage(img,0,0, canvas.width, Math.ceil(canvas.height*(img.height/img.width)));
+                    canvas_bak.width = canvas_bak.width;
+                    canvas_bak.height = Math.ceil(canvas.height*(img.height/img.width));
                 }else
-                    context.drawImage(img,0,0, canvas.width, canvas.width);
-                //alert(canvas.width/img.width);
-                //var previewHeight = img.height*(canvas.width/img.width);
+                    context.drawImage(img,0,0, canvas.width, canvas.height);
                 //context.drawImage(img,0,0);
+                cw = canvas.width;
+                ch = canvas.height;
             }
             $("#image-canvas").css('display','block');
         };
 
         pick.onerror = function () {
-                
+            alert("Image loading error.");
         };
+    });
+    $("#compare").click(function(e){
+        if (!rotating) {
+            rotating = true;            
+            // store current data to an image
+            myImage = new Image();
+            myImage.src = canvas_bak.toDataURL();
+            myImage.onload = function () {
+                //alert("yoooo");
+                var context = canvas.getContext("2d");
+                canvas.width = ch;
+                canvas.height = cw;
+                cw = canvas.width;
+                ch = canvas.height;
+                context.save();
+                context.translate(cw, ch / cw);
+                context.rotate(Math.PI / 2);
+                // draw the previows image, now rotated
+                context.drawImage(myImage, 0, 0);               
+                context.restore();
+               
+                // clear the temporary image
+                myImage = null;
+               
+                rotating = false;
+            }
+        }
     });
 })();
